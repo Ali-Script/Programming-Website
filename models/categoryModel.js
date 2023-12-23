49b16aad087ade103a4758b452f4dbce04a82e36
Ali-Script <ali.prg01@gmail.com>
2023-12-23 23:45:58 +0330

    add categoryModel

---
+++ models/categoryModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+    title: {
+        type: 'string',
+        required: true
+    },
+    href: {
+        type: 'string',
+        required: true
+    }
+
+}, { timestamps: true })
+const model = mongoose.model("Categories", schema);
+
+module.exports = model;
