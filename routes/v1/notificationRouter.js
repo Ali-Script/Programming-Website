aa3083db1ac8e06ff5cc1000bed4130b106be4f8
Ali-Script <ali.prg01@gmail.com>
2023-12-28 22:33:37 +0330

    notificationRouter.js

---
+++ routes/v1/notificationRouter.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const notificationController = require('./../../controller/v1/notificationController')
+const authMiddleware = require("./../../middlewares/authMiddleware");
+const isAdmin = require("./../../middlewares/isAdmin");
+
+const router = express.Router();
+
+router
+    .route("/send")
+    .post(authMiddleware, isAdmin, notificationController.send)
+router
+    .route("/get")
+    .get(authMiddleware, isAdmin, notificationController.get)
+router
+    .route("/getAll")
+    .get(authMiddleware, isAdmin, notificationController.getAll)
+
+module.exports = router;
